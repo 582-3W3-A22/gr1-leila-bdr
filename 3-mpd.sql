c40523dc33b862e5008404a746d8d6fd189a726d
Camille Semaan <csemaan@cmaisonneuve.qc.ca>
2022-11-07 11:10:31 -0500

    Implémentation dans MySQL (MPD)

---
+++ 3-mpd.sql
@@ -0,0 +1,85 @@
+-- ---
+-- Globals
+-- ---
+
+-- SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+-- SET FOREIGN_KEY_CHECKS=0;
+
+
+CREATE DATABASE leila;
+USE leila;
+
+
+-- ---
+-- Table 'categorie'
+-- 
+-- ---
+
+DROP TABLE IF EXISTS `categorie`;
+		
+CREATE TABLE `categorie` (
+  `id` TINYINT NOT NULL AUTO_INCREMENT,
+  `nom` VARCHAR(50) NOT NULL,
+  `type` ENUM('plat','vin') NOT NULL,
+  PRIMARY KEY (`id`)
+);
+
+-- ---
+-- Table 'plat'
+-- 
+-- ---
+
+DROP TABLE IF EXISTS `plat`;
+		
+CREATE TABLE `plat` (
+  `id` SMALLINT NOT NULL AUTO_INCREMENT,
+  `nom` VARCHAR(200) NOT NULL,
+  `description` VARCHAR(500) NULL DEFAULT NULL,
+  `portion` TINYINT NOT NULL DEFAULT 1,
+  `prix` DECIMAL(5,2) NOT NULL,
+  `categorie_id` TINYINT NOT NULL,
+  PRIMARY KEY (`id`)
+);
+
+-- ---
+-- Table 'vin'
+-- 
+-- ---
+
+DROP TABLE IF EXISTS `vin`;
+		
+CREATE TABLE `vin` (
+  `id` SMALLINT NOT NULL AUTO_INCREMENT,
+  `nom` VARCHAR(200) NOT NULL,
+  `description` VARCHAR(500) NULL DEFAULT NULL,
+  `provenance` VARCHAR(25) NOT NULL,
+  `prix` DECIMAL(6,2) NULL DEFAULT NULL,
+  `categorie_id` TINYINT NOT NULL,
+  PRIMARY KEY (`id`)
+);
+
+-- ---
+-- Foreign Keys 
+-- ---
+
+ALTER TABLE `plat` ADD FOREIGN KEY (categorie_id) REFERENCES `categorie` (`id`);
+ALTER TABLE `vin` ADD FOREIGN KEY (categorie_id) REFERENCES `categorie` (`id`);
+
+-- ---
+-- Table Properties
+-- ---
+
+-- ALTER TABLE `categorie` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE `plat` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE `vin` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- ---
+-- Test Data
+-- ---
+
+-- INSERT INTO `categorie` (`id`,`nom`,`type`) VALUES
+-- ('','','');
+-- INSERT INTO `plat` (`id`,`nom`,`description`,`portion`,`prix`,`categorie_id`) VALUES
+-- ('','','','','','');
+-- INSERT INTO `vin` (`id`,`nom`,`description`,`provenance`,`prix`,`categorie_id`) VALUES
+-- ('','','','','','');
